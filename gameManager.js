b7c2815f14fdb851db0b1bdc772e90032d229db0
feticcio <feticcio@pop-os.localdomain>
2020-08-02 13:24:32 +0200

    adding Fn

--- gameManager.js
+++ gameManager.js
@@ -1,8 +1,19 @@
+// ************
+// RESTART GAME
+//-------------
+const resetGame = () => window.location.reload();
+
+// **************************
+// CALLS Fn TO START THE GAME
+// --------------------------
 const setGameStart = (classType) => {
   setPlayer(classType);
   setFight();
 };
 
+// ******************************
+// SETS SELECTED PLAYER ON SCREEN
+// ------------------------------
 const setPlayer = (classType) => {
   const getPlayerStats = document.querySelector(".player-stats");
   let selectedPlayer = mobs.filter((mob) => mob.classe === classType);
@@ -17,15 +28,20 @@ const setPlayer = (classType) => {
   getPlayerStats.innerHTML = `${player}`;
 };
 
+// *************************
+// SETS THE ARENA FOR BATTLE
+//--------------------------
 const setFight = () => {
   const championList = document.querySelector(".champion-list");
   const getHeaderActions = document.querySelector(".header-action");
   const arena = document.querySelector(".arena");
+  const stats = document.querySelector(".stats");
   arena.style.visibility = "visible";
+  stats.style.visibility = "visible";
   championList.innerHTML = "";
   getHeaderActions.innerHTML = `
   <p>Get ready!</p>
   <button>start</button>
-  <button>reset</button>
+  <button onclick="resetGame()">reset</button>
   `;
 };
