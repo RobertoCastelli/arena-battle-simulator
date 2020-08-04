178b84f0f62d416e8a04ec32092cd4ae1d235e89
feticcio <feticcio@pop-os.localdomain>
2020-08-04 23:51:04 +0200

    organized fight scene and random enemy

---
+++ battle.js
@@ -0,0 +1,64 @@
+// **************
+// FIGHT SEQUENCE
+// --------------
+
+const attack = () => {
+  playerAttack();
+  checkDeath(selectedEnemy);
+  setEnemyStats();
+  setDelay(2000).then(() => {
+    enemyAttack();
+    checkDeath(selectedPlayer);
+    setPlayerStats();
+    checkVictory();
+  });
+};
+
+// **********************
+// PLAYER ATTACK SEQUENCE
+// ----------------------
+const playerAttack = () => {
+  let playerDamage = diceRoll(selectedPlayer[0].strength);
+  getPlayerScore.innerHTML = `${selectedPlayer[0].name} hits for: ${playerDamage}`;
+  selectedEnemy[0].health -= playerDamage;
+};
+
+// *********************
+// ENEMY ATTACK SEQUENCE
+//---------------------
+const enemyAttack = () => {
+  let enemyDamage = diceRoll(selectedEnemy[0].strength);
+  getEnemyScore.innerHTML = `${selectedEnemy[0].name} hits for: ${enemyDamage}`;
+  selectedPlayer[0].health -= enemyDamage;
+};
+
+// *************************
+// DEATH SEQUENCE
+
+// CHECK HEALTH NOT NEGATIVE
+// INJECT R.I.P. ICON
+// START NEXT ENEMY ROUND
+// -------------------------
+const checkDeath = (champion) => {
+  if (champion[0].health <= 0) {
+    champion[0].health = 0;
+    champion[0].icon = "./images/rip.png";
+    champion[0].status = "dead";
+    setDelay(2000).then(() => setArena());
+  }
+};
+
+// ********************
+// CHECK VICOTRY STATUS
+// --------------------
+const checkVictory = () => {
+  if (selectedPlayer[0].health <= 0 && selectedEnemy[0].health <= 0) {
+    getScoreResult.innerHTML = `You both Die!`;
+  } else if (selectedPlayer[0].health <= 0 && selectedEnemy[0].health > 0) {
+    getScoreResult.innerHTML = `${selectedEnemy[0].name} slays ${selectedPlayer[0].name}, You Lose!`;
+  } else if (selectedEnemy[0].health <= 0 && selectedPlayer[0].health > 0) {
+    getScoreResult.innerHTML = `${selectedPlayer[0].name} slays ${selectedEnemy[0].name}, You Win!`;
+  } else {
+    getScoreResult.innerHTML = `Keep Fighting!`;
+  }
+};
--- functions.js
+++ functions.js
@@ -1,16 +1,36 @@
-// *******************
-// GET SELECTED PLAYER
-// -------------------
+// ************
+// RESTART GAME
+//-------------
+const resetGame = () => window.location.reload();
+
+// *********
+// DICE ROLL
+// ---------
+const diceRoll = (power) => Math.floor(Math.random() * power);
+
+// ***********
+// SET A DELAY
+// -----------
+const setDelay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+// ****************************
+// GET SELECTED PLAYER AT START
+// ----------------------------
 const getPlayer = (classType) => {
   selectedPlayer = players.filter((mob) => mob.classe === classType);
   return selectedPlayer;
 };
 
-// *********
-// GET ENEMY
-// ---------
+// *************************
+// GET ENEMY FROM ALIVE POOL
+// -------------------------
 const getEnemy = () => {
-  selectedEnemy = mobs.map((mob) => mob);
+  getAliveEnemy = mobs.filter((mob) => mob.status === "alive");
+  let counter = diceRoll(getAliveEnemy.length);
+  selectedEnemy = new Array(getAliveEnemy[counter]);
+  return selectedEnemy;
 };
 
 // *****************************
@@ -45,76 +65,4 @@ const setPlayerStats = () => {
     `;
   });
   getPlayerStats.innerHTML = `${playerNew}`;
-};
-
-// ************
-// RESTART GAME
-//-------------
-const resetGame = () => window.location.reload();
-
-// CHECK SPEED -> WHO ATTACKS FIRST
-// ATTACK SEQUENCE Att/Def -> Def/Att -> CHECK DEATH
-// CHECK ENERGY -> WHO ATTACKS
-// ATTACK SEQUENCE Att/Def -> Def/Att -> CHECK DEAT
-
-// ****************
-// DICE ROLL RANDOM
-// ----------------
-const diceRoll = (power) => Math.floor(Math.random() * power);
-
-// ***********
-// CHECK DEATH
-// -----------
-const checkNegativeHp = (champion) => {
-  if (champion[0].health <= 0) champion[0].health = 0;
-};
-
-const checkVictory = () => {
-  if (selectedPlayer[0].health <= 0 && selectedEnemy[0].health <= 0) {
-    getScoreResult.innerHTML = `You both Die!`;
-  } else if (selectedPlayer[0].health <= 0 && selectedEnemy[0].health > 0) {
-    getScoreResult.innerHTML = `${selectedEnemy[0].name} slays ${selectedPlayer[0].name}, You Lose!`;
-  } else if (selectedEnemy[0].health <= 0 && selectedPlayer[0].health > 0) {
-    getScoreResult.innerHTML = `${selectedPlayer[0].name} slays ${selectedEnemy[0].name}, You Win!`;
-  } else {
-    getScoreResult.innerHTML = `Keep Fighting!`;
-  }
-};
-
-// **********************
-// PLAYER ATTACK SEQUENCE
-// ----------------------
-const playerAttack = () => {
-  let playerDamage = diceRoll(selectedPlayer[0].strength);
-  getPlayerScore.innerHTML = `${selectedPlayer[0].name} hits for: ${playerDamage}`;
-  selectedEnemy[0].health -= playerDamage;
-};
-
-// *********************
-// ENEMY ATTACK SEQUENCE
-//---------------------
-const enemyAttack = () => {
-  let enemyDamage = diceRoll(selectedEnemy[0].strength);
-  getEnemyScore.innerHTML = `${selectedEnemy[0].name} hits for: ${enemyDamage}`;
-  selectedPlayer[0].health -= enemyDamage;
-};
-
-const delay = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-// ***********
-// FIGHT SCENE
-// -----------
-
-const attack = () => {
-  playerAttack();
-  checkNegativeHp(selectedEnemy);
-  setEnemyStats();
-  delay(2000).then(() => {
-    enemyAttack();
-    checkNegativeHp(selectedPlayer);
-    setPlayerStats();
-    checkVictory();
-  });
 };
